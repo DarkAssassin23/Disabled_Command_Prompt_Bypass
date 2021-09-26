43423ae0a976016ffbfb82002624207d5d32934f
DarkAssassin23 <wi77jones@aol.com>
2021-09-25 21:53:47 -0400

    Added cmd.bat

---
+++ cmd.bat
@@ -0,0 +1,25 @@
+@echo off
+ver
+echo (c) Microsoft Corporation. All rights reserved.
+echo.
+set currentPath="%userprofile%"
+cd /d %currentPath%
+
+set /p command="%CD%>"
+if "%command%" == "exit" (goto 'close')
+cmd.exe /c "%command% & cd > %userprofile%\pwd.txt"
+echo.
+goto 'prompt'
+
+: 'prompt'
+set /p currentPath=<%userprofile%\pwd.txt
+cd /d %currentPath%
+set /p command="%CD%>"
+if "%command%" == "exit" (goto 'close')
+cmd.exe /c "%command% & cd > %userprofile%\pwd.txt"
+echo.
+goto 'prompt'
+
+: 'close'
+if exist  %userprofile%\pwd.txt del %userprofile%\pwd.txt
+exit
