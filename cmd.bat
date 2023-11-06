b5e33a21150020ac0219885e5188db24cf3bc801
DarkAssassin23 <15916504+DarkAssassin23@users.noreply.github.com>
2023-11-06 18:45:02 -0500

    Fully fixed issues with quotes in commands

--- cmd.bat
+++ cmd.bat
@@ -1,24 +1,28 @@
 @echo off
-ver
+setlocal enabledelayedexpansion
+
+:: Get the current Windows build version and print it
+ver > %userprofile%\version.txt
+for /f "delims=" %%x in (%userprofile%\version.txt) do set Build=%%x
+rm %userprofile%\version.txt
+echo %Build%
 echo (c) Microsoft Corporation. All rights reserved.
 echo.
-set currentPath="%userprofile%"
-cd /d %currentPath%
 
-set /p input="%CD%>"
-set command=%input:"=""%
-if "%command%" == "exit" (goto 'close')
-cmd.exe /c "%command% & cd > %userprofile%\pwd.txt"
-echo.
-goto 'prompt'
+:: Set the users home directory as the present working directory
+echo %userprofile% > %userprofile%\pwd.txt
+
+set setPathCMD=" & cd > %userprofile%\pwd.txt"
+:: Strip the quotes
+set setPathCMD=!setPathCMD:~1,-1!
 
 : 'prompt'
 set /p currentPath=<%userprofile%\pwd.txt
 cd /d %currentPath%
 set /p input="%CD%>"
-set command=%input:"=""%
-if "%command%" == "exit" (goto 'close')
-cmd.exe /c "%command% & cd > %userprofile%\pwd.txt"
+set "command=%input:"="%"
+if !command! == exit (goto 'close')
+cmd.exe /c !command!!setPathCMD!
 echo.
 goto 'prompt'
 
